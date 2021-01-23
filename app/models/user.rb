e87ee8b02b883f1d8aed004c55db55dfa0f0eecf
Tsutomu-sasaki1 <tsutomusasaki1209@yahoo.co.jp>
2021-01-23 21:45:34 +0900

    usersテーブル、カラムの作成

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :histories,         dependent: :destroy
+  has_many :items,             dependent: :destroy
+
+  validates :nickname,         presence: true
+  validates :name_first,       presence: true
+  validates :name_second,      presence: true
+  validates :name_kana_first,  presence: true
+  validates :name_kana_second, presence: true
+  validates :birthday,         presence: true
+
+end
