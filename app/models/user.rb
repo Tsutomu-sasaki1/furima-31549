2c7e5e36f81a800a43fc44408612a453c0f3aa7f
Tsutomu-sasaki1 <tsutomusasaki1209@yahoo.co.jp>
2021-01-30 15:00:17 +0900

    新規ユーザー登録　単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,19 @@ class User < ApplicationRecord
   has_many :histories,         dependent: :destroy
   has_many :items,             dependent: :destroy
 
-  validates :nickname,         presence: true
-  validates :name_first,       presence: true
-  validates :name_second,      presence: true
-  validates :name_kana_first,  presence: true
-  validates :name_kana_second, presence: true
   validates :birthday,              presence: true
+  validates :nickname,              presence: true
 
+  # 全角ひらがな、全角カタカナ、漢字（鬼車）のみ
+  NAME_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/.freeze
+  validates_format_of :name_first, :name_second,            presence: true, with: NAME_REGEX
+
+  # 全角カタカナのみ
+  NAME_KANA_REGEX =/\A[ァ-ヶー－]+\z/.freeze
+  validates_format_of :name_kana_first, :name_kana_second,  presence: true, with: NAME_KANA_REGEX
+
+  #半角英数字混合のみ
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
+  
 end
