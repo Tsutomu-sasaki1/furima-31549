e77e8787cfe43eea27595d81e39b2f8cdc9ba379
Tsutomu-sasaki1 <tsutomusasaki1209@yahoo.co.jp>
2021-02-15 15:19:57 +0900

    不要なテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_050327) do
+ActiveRecord::Schema.define(version: 2021_02_15_061538) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,16 +46,6 @@ ActiveRecord::Schema.define(version: 2021_02_14_050327) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "category", null: false
-    t.integer "condition", null: false
-    t.integer "date_of_shipment", null: false
-    t.integer "prefectures", null: false
-    t.integer "shipping_charge", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
