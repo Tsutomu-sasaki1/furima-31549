0da64dacd0ff4cdf9ed0c88b89a8c6c08ef329a1
Tsutomu-sasaki1 <tsutomusasaki1209@yahoo.co.jp>
2021-02-16 13:24:20 +0900

    migrationファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_061538) do
+ActiveRecord::Schema.define(version: 2021_01_31_040613) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
