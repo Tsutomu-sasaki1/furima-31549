3b663b28349b52aa1c533f645f9d957f85a803c3
Tsutomu-sasaki1 <tsutomusasaki1209@yahoo.co.jp>
2021-02-17 15:43:20 +0900

    edit,updateアクション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new,:edit]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -22,11 +22,21 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def
-  # end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
-  # def
-  # end
+  def edit
+    @item = Item.find(params[:id])
+    unless @item.user.id == current_user.id
+      redirect_to root_path
+    end
+  end
 
   # def
   # end
@@ -40,4 +50,5 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :category_id, :condition_id, :prefecture_id, :date_of_shipment_id, :price,
                                  :shipping_charge_id, :explanation).merge(user_id: current_user.id)
   end
+
 end
