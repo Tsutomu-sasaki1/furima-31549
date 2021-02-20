07902e814745417a59ba1fac494b2dc9d023c229
Tsutomu-sasaki1 <tsutomusasaki1209@yahoo.co.jp>
2021-02-20 16:38:27 +0900

    payjp登録　トークン化処理まで

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,4 +3,14 @@ class AddressesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  # def create
+    # @addresse = Addresse.new
+  # end
+# 
+  # private
+# 
+  # def addresse_params
+    # params.require(:addresse).permit(:postal_code, :city, :address, :phone_number, :prefecture_id).merge(user_id: current_user.id)
+  # end
 end
