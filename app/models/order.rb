71e8a1706efe4896e14f4e9e713ac990b85c7baf
Tsutomu-sasaki1 <tsutomusasaki1209@yahoo.co.jp>
2021-02-28 15:34:50 +0900

    validatesヘルパー追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,11 +6,14 @@ class Order
     validates :postal_code
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A\d{11}\z/ }
-    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :phone_number
+    validates :prefecture_id
   end
-    validates :token, presence: { message: "Token can't be blank" }
-
+  
+  validates :phone_number, format: { with: /\A\d{11}\z/ }
+  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :token, presence: { message: "can't be blank" }
   def save
     history = Historie.create(user_id: user_id, item_id: item_id)
     Addresse.create!(postal_code: postal_code, city: city, address: address, phone_number: phone_number, prefecture_id: prefecture_id,building: building, history_id: history.id)
