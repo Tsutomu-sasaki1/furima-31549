54929b551c7f39a9384dfe4347444a232d9305f6
Tsutomu-sasaki1 <tsutomusasaki1209@yahoo.co.jp>
2021-03-06 15:41:01 +0900

    文字列として修正

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     postal_code     { '123-4567' }
     city            { '横浜区' }
     address         { '青山1-1' }
-    phone_number    { 12345678901 }
+    phone_number    { '12345678901' }
     building        { 'ビル' }
     prefecture_id   { 1 }
     token           {"tok_5435c85088478825734a833b6e28"}
